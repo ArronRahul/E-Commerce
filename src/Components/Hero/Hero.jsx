28ea1cb9aa19f579bc3b2a922dddcbe1c71814d4
RAHUL A P <rahul.ap@aot-technologies.com>
2024-07-25 05:44:05 -0700

    second-comit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -11,17 +11,18 @@ const Hero=()=> {
             <h2> NEW ARRIVALS ONLY</h2>
             <div>
                 <div className='hand-hand-icon'>
-                    <>new</>
-                    <img src={hand_icon} alt=''/>
+                    <p>new</p>
+                    <img className='hand-icon' src={hand_icon} alt=''/>
                 </div>
             </div>
             <p>collection</p>
             <p>for everyone</p>
+            <div className='hero-latest-btn'>
+                <div>Latest collection</div>
+                <img src={arrw_icon} alt=''/>
+            </div>
         </div>
-        <div className='hero-latest-btn'>
-            <div>Latest collection</div>
-            <img src={arrw_icon} alt=''/>
-        </div>
+        
         <div className='hero-right'>
             <img src={hero_img} alt=''/>
         </div>
