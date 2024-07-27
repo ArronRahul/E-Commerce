df4cd65230221cd1c24283e1c5265fbc1b61047e
RAHUL A P <rahul.ap@aot-technologies.com>
2024-07-27 02:33:38 -0700

    home-page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ShopCat from './Pages/ShopCat'
 import Productpage from './Pages/Productpage'
 import Cart from './Pages/Cart'
 import LoginSignup from './Pages/LoginSignup'
+import Footer from './Components/Footer/Footer'
 
 function App() {
 
@@ -24,6 +25,7 @@ function App() {
         <Route path='/cart' element={<Cart />} />
         <Route path='/login' element={<LoginSignup />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   </div>
   );
--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -3,7 +3,14 @@ import './NewLetter.css'
 
 function NewsLetter() {
   return (
-    <div>NewsLetter</div>
+    <div className='newletter'>
+      <h1>Get Exclusive offers On Your Email</h1>
+      <p>subscribe to our newletter and stay updated</p>
+      <div>
+        <input type="email" placeholder=' Your Email Id'/>
+        <button>Subscribe</button>
+      </div>
+    </div>
     
   )
 }
